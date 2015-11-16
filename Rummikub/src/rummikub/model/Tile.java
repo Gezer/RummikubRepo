eedb77dcdd4df9395bb211045e0030ede4b8c17e
Gezer <guydunski@gmail.com>
2015-11-16 14:25:19 +0200

    Tile completed

--- Rummikub/src/rummikub/model/Tile.java
+++ Rummikub/src/rummikub/model/Tile.java
@@ -15,25 +15,44 @@ import java.util.*;
 public class Tile {
     
     private final Color m_color;
-    private final int value;
     
-    private Tile(Color color, int value) {
-        this.m_color = color;
-        this.value = value;
-    }
-    
-    private static final List<Tile> deck = new ArrayList<Tile>(); 
-
-    static{
-        
-    }
-    }
+    private final int m_value;
     
     public Color getColor() {
         return m_color;
     }
 
     public int getValue() {
-        return value;
+        return m_value;
+    }
+    
+    private Tile(Color color, int value) {
+        this.m_color = color;
+        this.m_value = value;
+    }
+    
+    private static final List<Tile> tileSet = new ArrayList<Tile>(); 
+
+    private static final int MIN_VALUE = 1;
+    private static final int MAX_VALUE = 13;
+    
+    static
+    {
+        for (Color color : Color.values())
+        {
+            for (int i = MIN_VALUE; i <= MAX_VALUE; i++) {
+                tileSet.add(new Tile(color, i));
+                tileSet.add(new Tile(color, i));
+            }
+        }
+        
+        //adding the Joker: represented by 0
+        tileSet.add(new Tile(Color.RED, 0));
+        tileSet.add(new Tile(Color.BLACK, 0));
+    }
+        
+    public static ArrayList<Tile> newTileSet()
+    {
+        return new ArrayList<Tile>(tileSet);
     }
 }
